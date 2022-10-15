21d0efe23a01f0138e588b581c4cadb214fbe927
Christopher <Chris1Bobsy@Gmail.com>
2022-10-15 18:00:16 +0100

    setup app page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Navbar from "./components/Navbar";
+
+import { lightTheme } from "./theme";
+
+const AppWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.colors.main.background};
+`;
 
 function App() {
-  return <div className="App"></div>;
+  const [isThemeLight, setIsThemeLight] = useState(true);
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <AppWrapper>
+        <Navbar />
+      </AppWrapper>
+    </ThemeProvider>
+  );
 }
 
 export default App;
