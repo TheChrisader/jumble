949af06cd5f250139dcff011f3140651b083fed0
Christopher <Chris1Bobsy@Gmail.com>
2022-10-15 17:59:13 +0100

    create theme object

---
+++ client/src/theme.ts
@@ -0,0 +1,36 @@
+export const lightTheme = {
+  mode: "light",
+  colors: {
+    main: {
+      primary: {
+        default: "#651fff",
+        light: "#7c4dff",
+        dark: "#6200ea",
+        border: "#dfe3fa",
+        background: "#7c5dfa2d",
+      },
+      success: {
+        default: "#19c289",
+        light: "#00e676",
+        dark: "#388e3c",
+        border: "#2caf9e",
+        background: "#2cd69d18",
+      },
+      danger: {
+        default: "#d50000",
+        light: "#e53935",
+        dark: "#b71c1c",
+        border: "#af2121",
+        background: "#ec57571c",
+      },
+      background: "#F8F8FB",
+      white: "#FFFFFF",
+    },
+    text: {
+      primary: "#0C0E16",
+      secondary: "#71727a",
+      white: "#FFFFFF",
+      black: "#212121",
+    },
+  },
+};
