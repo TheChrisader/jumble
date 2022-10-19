8b7a137d2e56505feaea33ef0ae45a03c2d7f0b3
Christopher <Chris1Bobsy@Gmail.com>
2022-10-19 19:11:42 +0100

    create modal store

---
+++ client/src/store/modalStore.ts
@@ -0,0 +1,39 @@
+import create from "zustand";
+import { devtools, persist } from "zustand/middleware";
+import { ITask } from "../utils/types/DataTypes";
+
+interface IModal {
+  type: string;
+  detail?: ITask;
+  statusArr?: string[];
+  boardTab?: string;
+}
+
+const initialState: IModal = {
+  type: "",
+  detail: {
+    id: "",
+  },
+  statusArr: [],
+  boardTab: "",
+};
+
+export const useModalStore = create(
+  devtools(
+    persist(
+      (set) => ({
+        type: initialState.type,
+        detail: initialState.detail,
+        statusArr: initialState.statusArr,
+        boardTab: initialState.boardTab,
+
+        openModal: (payload: IModal) =>
+          set((state: any) => ({ ...state, ...payload })),
+
+        closeModal: () => set({ type: "" }),
+      }),
+      { name: "modal" }
+    ),
+    { name: "modalStore" }
+  )
+);
